1dd376f41d271f264cbde12694b7f93791042286
Alex Vlasov <drumlife182@gmail.com>
2022-09-07 14:23:55 +0200

    some changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.17.1"
 
 set :application, "drop"
 set :repo_url, "git@github.com:alexvlasov182/drop.git"
+set :branch, "main"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
